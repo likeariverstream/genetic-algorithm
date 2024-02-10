c17ae01d064f5fe0edd4ca4124ffbfa9d273c4a3
Sergey Kalachev <rollstream@yandex.ru>
2024-02-10 20:36:53 +0700

    code style fixed

--- src/main/java/algorithm/Chromosome.java
+++ src/main/java/algorithm/Chromosome.java
@@ -1,15 +1,17 @@
 package algorithm;
+
 import java.util.List;
-    public abstract class Chromosome<T extends Chromosome<T>> implements Comparable<T> {
-        public abstract double fitness();
 
-        public abstract List<T> crossover(T other);
+public abstract class Chromosome<T extends Chromosome<T>> implements Comparable<T> {
+    public abstract double fitness();
 
-        public abstract void mutate();
+    public abstract List<T> crossover(T other);
 
-        public abstract T copy();
+    public abstract void mutate();
 
-        @Override
+    public abstract T copy();
+
+    @Override
     public int compareTo(T other) {
         Double mine = other.fitness();
         Double theirs = other.fitness();
